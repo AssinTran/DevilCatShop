38b01d3bfe5799cb58489e2b3bc2eeb79e94540b
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-12-14 22:04:59 +0700

    Add DAO class

---
+++ DataAccess/DAO.cs
@@ -0,0 +1,26 @@
+﻿namespace DataAccess
+{
+    public class DAO<T> where T : class, new()
+    {
+        private static T? _instance;
+        private static readonly object _lock = new object();
+
+        public DAO()
+        {
+            DB = new DevilCatContext();
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instance ?? new T { };
+                }
+            }
+        }
+
+        public DevilCatContext DB { get; set; }
+    }
+}
