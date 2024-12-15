d7a2dd157b63aebd162cb061faa865a086124067
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-12-15 17:05:24 +0700

    add generate id class

---
+++ DataAccess/GenerateID.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DataAccess
+{
+    public class GenerateID
+    {
+        public string Value { get; set; }
+
+        public GenerateID(string inputValue)
+        {
+            Value = GenerateId(inputValue);
+        }
+
+        private string GenerateId(string temp)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                //chuyen chuoi thanh mang byte
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(temp));
+                //chuyen mang byte thanh dang hex
+                StringBuilder rs = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    rs.Append(bytes[i].ToString("x2"));
+                }
+                // Tra ve chuoi 32 ky tu dau tien
+                return rs.ToString().Substring(0, 32);
+            }
+        }
+
+    }
+}
